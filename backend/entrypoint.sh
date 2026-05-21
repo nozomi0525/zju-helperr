51188549c554e1f3ed897740e7867c37c9acd9ce
101315ddd <3240105647@zju.edu.cn>
2026-05-21 13:11:24 +0800

    Revert "解决docker查看问题"

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 set -e
-
 echo "Making migrations and running migrate..."
 python manage.py makemigrations --noinput || true
 python manage.py migrate --noinput
-
 echo "Collect static..."
 python manage.py collectstatic --noinput
-
 echo "Starting server..."
 gunicorn campus_help.wsgi:application --bind 0.0.0.0:8000 --workers 1
